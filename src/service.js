f8fb810b6c236b55488a0b000bdca1ff0d30c127
Ayush Krishnatray <ayushkrishnatray@197NODMB25134.local>
2021-01-18 17:03:30 +0530

    chore: updating service file

--- src/service.js
+++ src/service.js
@@ -1,15 +1,23 @@
-const axios = require('axios');
-
 export const service = (reqObj) => {
   let { url } = reqObj;
   const { method = 'get', page } = reqObj;
   if (page) {
     url = `${url}?page=${page}`;
   }
-  return axios({
-    method,
-    url
-  }).then((response) => {
-    return response && response.data;
-  });
+  return fetch(url, {
+    method
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.text().then((res) => {
+          return JSON.parse(res);
+        });
+      }
+      return response.text().then((res: any) => {
+        return Promise.reject(res);
+      });
+    })
+    .catch((error) => {
+      return Promise.reject(error);
+    });
 };
